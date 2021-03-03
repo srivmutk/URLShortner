76dbca869f92a20005f6581e7073470b4d883d9b
Sysnomid <sam@sysnomid.com>
2021-03-02 21:57:38 -0500

    main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,17 +14,20 @@ dotenv.config();
     const app = express()
     app.use(express.json());
     app.use(express.urlencoded({ extended: true })); 
-      
-    app.get("/", (_req, res, next) => {
-        res.send("URL SHORTENER API")
-        res.header("Access-Control-Allow-Origin", "https://6nmd.us");
+
+    app.use(function(_req, res, next) {
+        res.header("Access-Control-Allow-Origin", "6nmd.us");
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type')        
         next();
+      });
+      
+    app.get("/", (_req, res) => {
+        res.send("URL SHORTENER API")
     });
 
-    app.get("/:id", async (req, res, next) => {
+    app.get("/:id", async (req, res) => {
         const slug = await UrlShortenerData.findOne(
             { where:
                 { UrlId: req.params.id }
@@ -36,14 +39,9 @@ dotenv.config();
         else if(!slug) {
             res.sendStatus(404);
         }
-        res.header("Access-Control-Allow-Origin", "https://6nmd.us");
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type')        
-        next();
     });
 
-    app.post("/register_url", async (req, res, next) => {
+    app.post("/register_url", async (req, res) => {
         let data = req.body
         let id = nanoid(5)
         try {
@@ -57,11 +55,6 @@ dotenv.config();
         } catch {
             res.sendStatus(404)
         }
-        res.header("Access-Control-Allow-Origin", "https://6nmd.us");
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type')        
-        next();
     });
 
     app.listen(4000)
