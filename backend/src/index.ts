2dfb7ba7948e099048d714d4d5c27b723454ce90
Sysnomid <sam@sysnomid.com>
2021-03-02 21:43:26 -0500

    main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,9 @@ import UrlShortenerData from "./entity/UrlShortenerData";
 import express from "express";
 import {nanoid} from 'nanoid';
 import {URL} from "url";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 (async () => {
     await createConnection()
@@ -11,7 +14,19 @@ import {URL} from "url";
     const app = express()
     app.use(express.json());
     app.use(express.urlencoded({ extended: true })); 
-  
+
+    app.use(function(_req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type')        
+        next();
+      });
+      
+    app.get("/", (_req, res) => {
+        res.send("URL SHORTENER API")
+    });
+
     app.get("/:id", async (req, res) => {
         const slug = await UrlShortenerData.findOne(
             { where:
