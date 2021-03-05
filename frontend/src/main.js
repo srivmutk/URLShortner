01087e41ab99d187f2ad1e56fc0c7bb8d302a62e
Sysnomid <sam@sysnomid.com>
2021-03-05 18:56:57 -0500

    main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import titleMixin from './mixins/titleMixin'
 
-Vue.mixin(titleMixin)
 Vue.config.productionTip = false
 
 new Vue({
